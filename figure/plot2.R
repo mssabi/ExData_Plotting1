c6028c69c9baaca40c5cec2c70f05174c04eafc7
mssabi <38126280+mssabi@users.noreply.github.com>
2018-06-27 12:48:21 +0530

    Add files via upload

---
+++ figure/plot2.R
@@ -0,0 +1,20 @@
+## read the input text file and load into data_entire 
+data_entire<-read.csv("household_power_consumption.txt",header=T, sep=';',na.strings="?",nrows=2075259, check.names=F,stringsAsFactors=F,comment.char="", quote='\"')
+
+## converting the date format 
+data_entire$Date <- as.Date(data_entire$Date, format="%d/%m/%Y")
+
+## subsetting the data to select only feb 1st and feb 2nd dates
+data <- subset(data_entire, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## creating the plot
+plot(data$Global_active_power~data$Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+
+## saving to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+
+dev.off()
